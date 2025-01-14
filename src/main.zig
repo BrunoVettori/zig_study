131cceabcdc89456d86f3aa990cd86d8356b9abf
Bruno Vettori <bruno_vettori@proton.me>
2025-01-14 10:26:50 -0300

    removing cache

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,20 @@
 const std = @import("std");
 
-const formater = @import("./utils.zig");
+pub const User = struct {
+    power: u64,
+    name: []const u8,
 
-// pub const User = struct {
-//     power: u64,
-//     name: []const u8,
-
-//     pub fn diagnose(user: User) void {
-//         if (user.power >= 8000) {
-//             std.debug.print("{s} porwe is {d}\n", .{ user.name, user.power });
-//         }
-//     }
-// };
+    pub fn diagnose(user: User) void {
+        if (user.power >= 8000) {
+            std.debug.print("{s} porwe is {d}\n", .{ user.name, user.power });
+        }
+    }
+};
 
 pub fn main() !void {
-    // var user = User{ .power = 8999, .name = "Bruno" };
+    var user = User{ .power = 8999, .name = "Bruno" };
 
-    // user.name = "Alex";
+    user.name = "Alex";
 
-    // user.diagnose();
-
-    var value: f64 = 0;
-
-    value = value + 10;
-
-    value = value / 3;
-
-    value = formater.FormatDecimals(value, "2");
-
-    std.debug.print("{d}\n", .{value});
+    user.diagnose();
 }
