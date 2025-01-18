ff1782c3c5502e86b7827da105230901ca5a9fd2
bruno_vettori <bruno_vettori@proton.me>
2025-01-17 21:28:48 -0300

    Who neeed a routes library, suck my balls express

--- src/main.zig
+++ src/main.zig
@@ -1,30 +1,36 @@
 const std = @import("std");
 const zap = @import("zap");
 
-fn on_request(r: zap.Request) void {
-    if (r.path) |the_path| {
-        std.debug.print("PATH: {s}\n", .{the_path});
+fn router(request: zap.Request) void {
+    const path: []const u8 = request.path orelse "/";
+
+    if (std.mem.eql(u8, path, "/")) {
+        const country: []const u8 = request.query orelse "";
+
+        const file = @embedFile("pages/index.html");
+
+        const allocator = std.heap.page_allocator;
+
+        const size = std.mem.replacementSize(u8, file, "{{country}}", country);
+        const output = allocator.alloc(u8, size) catch return;
+        _ = std.mem.replace(u8, file, "{{country}}", country, output);
+
+        defer allocator.free(output);
+
+        request.sendBody(output) catch return;
     }
 
-    const country: []const u8 = r.query orelse "";
+    if (std.mem.eql(u8, path, "/futebol")) {
+        const file = @embedFile("pages/futebol.html");
 
-    const file = @embedFile("pages/index.html");
-
-    const allocator = std.heap.page_allocator;
-
-    const size = std.mem.replacementSize(u8, file, "{{country}}", country);
-    const output = allocator.alloc(u8, size) catch return;
-    _ = std.mem.replace(u8, file, "{{country}}", country, output);
-
-    defer allocator.free(output);
-
-    r.sendBody(output) catch return;
+        request.sendBody(file) catch return;
+    }
 }
 
 pub fn main() !void {
     var listener = zap.HttpListener.init(.{
         .port = 3000,
-        .on_request = on_request,
+        .on_request = router,
         .log = true,
     });
 
