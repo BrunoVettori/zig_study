a47bb46aabddacece44eaa7b44ae879c6657f23e
Bruno Vettori <bruno_vettori@proton.me>
2025-01-20 10:38:12 -0300

    router

--- src/main.zig
+++ src/main.zig
@@ -1,42 +1,18 @@
 const std = @import("std");
 const zap = @import("zap");
 
-fn router(request: zap.Request) void {
-    const path: []const u8 = request.path orelse "/";
-
-    if (std.mem.eql(u8, path, "/")) {
-        const country: []const u8 = request.query orelse "";
-
-        const file = @embedFile("pages/index.html");
-
-        const allocator = std.heap.page_allocator;
-
-        const size = std.mem.replacementSize(u8, file, "{{country}}", country);
-        const output = allocator.alloc(u8, size) catch return;
-        _ = std.mem.replace(u8, file, "{{country}}", country, output);
-
-        defer allocator.free(output);
-
-        request.sendBody(output) catch return;
-    }
-
-    if (std.mem.eql(u8, path, "/futebol")) {
-        const file = @embedFile("pages/futebol.html");
-
-        request.sendBody(file) catch return;
-    }
-}
+const router = @import("./routes/router.zig");
 
 pub fn main() !void {
     var listener = zap.HttpListener.init(.{
-        .port = 3000,
-        .on_request = router,
+        .port = 3005,
+        .on_request = router.route,
         .log = true,
     });
 
     try listener.listen();
 
-    std.debug.print("Listening on 0.0.0.0:3000\n", .{});
+    std.debug.print("Listening on 0.0.0.0:3005\n", .{});
 
     // start worker threads
     zap.start(.{
---
+++ src/routes/router.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const zap = @import("zap");
+
+pub fn route(request: zap.Request) void {
+    const path: []const u8 = request.path orelse "/";
+
+    if (std.mem.eql(u8, path, "/")) {
+        const country: []const u8 = request.query orelse "";
+
+        const file = @embedFile("../pages/index.html");
+
+        const allocator = std.heap.page_allocator;
+
+        const size = std.mem.replacementSize(u8, file, "{{country}}", country);
+        const output = allocator.alloc(u8, size) catch return;
+        _ = std.mem.replace(u8, file, "{{country}}", country, output);
+
+        defer allocator.free(output);
+
+        request.sendBody(output) catch return;
+    }
+
+    if (std.mem.eql(u8, path, "/futebol")) {
+        const file = @embedFile("../pages/futebol.html");
+
+        request.sendBody(file) catch return;
+    }
+}
