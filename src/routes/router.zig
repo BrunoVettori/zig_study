5148ff0d73ec0e6180ae6fe3169961d42eec5bf0
Bruno Vettori <bruno_vettori@proton.me>
2025-01-20 17:25:50 -0300

    safe

--- src/routes/router.zig
+++ src/routes/router.zig
@@ -5,24 +5,34 @@ pub fn route(request: zap.Request) void {
     const path: []const u8 = request.path orelse "/";
 
     if (std.mem.eql(u8, path, "/")) {
-        const country: []const u8 = request.query orelse "";
+        const language: []const u8 = request.query orelse "";
 
         const file = @embedFile("../pages/index.html");
 
         const allocator = std.heap.page_allocator;
 
-        const size = std.mem.replacementSize(u8, file, "{{country}}", country);
+        const size = std.mem.replacementSize(u8, file, "{{language}}", language);
         const output = allocator.alloc(u8, size) catch return;
-        _ = std.mem.replace(u8, file, "{{country}}", country, output);
+        _ = std.mem.replace(u8, file, "{{language}}", language, output);
 
         defer allocator.free(output);
 
         request.sendBody(output) catch return;
     }
 
-    if (std.mem.eql(u8, path, "/futebol")) {
-        const file = @embedFile("../pages/futebol.html");
+    if (std.mem.eql(u8, path, "/front")) {
+        const language: []const u8 = request.query orelse "";
 
-        request.sendBody(file) catch return;
+        const file = @embedFile("../pages/front.html");
+
+        const allocator = std.heap.page_allocator;
+
+        const size = std.mem.replacementSize(u8, file, "{{language}}", language);
+        const output = allocator.alloc(u8, size) catch return;
+        _ = std.mem.replace(u8, file, "{{language}}", language, output);
+
+        defer allocator.free(output);
+
+        request.sendBody(output) catch return;
     }
 }
